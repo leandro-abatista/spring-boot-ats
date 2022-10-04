a57039cdde12fb1ba01a7cc1ffe939eefcb76da3
Leandro Amorim <leandroamorimpb@gmail.com>
2022-10-03 22:34:55 -0300

    Carregando a lista de pessoas cadastradas no banco de dados, melhorando o salvar e carregando os dados em tela

--- src/main/java/com/curso/springboot/controller/PessoaController.java
+++ src/main/java/com/curso/springboot/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.curso.springboot.classes.model.Pessoa;
 import com.curso.springboot.repositories.PessoaRepository;
@@ -14,14 +15,38 @@ public class PessoaController {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
+	/**
+	 * Método de início
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public String inicio() {
 		return "cadastro/cadastropessoa";
 	}
 	
+	/**
+	 * Método para salvar uma pessoa
+	 * @param pessoa
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		return "cadastro/cadastropessoa";
+		
+		ModelAndView view = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoas = pessoaRepository.findAll();
+		view.addObject("pessoas", pessoas);
+		
+		return view;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+		
+		ModelAndView view = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoas = pessoaRepository.findAll();
+		view.addObject("pessoas", pessoas);
+		
+		return view;
 	}
 }
